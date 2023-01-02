cd3ca33d20b2f4e795b0f707db20c579173d8488
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2023-01-02 15:15:22 +0100

    start weather function and api

---
+++ pgm_2_opdracht_1_deniel_jef/app/static/js/main.js
@@ -0,0 +1,58 @@
+(() => {
+const app = {
+    initialize() {
+    // cache
+    this.cacheElements();
+    // register listeners
+    this.registerListeners();
+    // update UI,
+    this.updateUI();
+    },
+  cacheElements() {
+  },
+  registerListeners() {
+  },
+  async updateUI() {
+  },
+ };
+ app.initialize();
+})();
+
+// Mogelijke functies om jullie op weg te helpen
+// Hier zijn enkele functienamen om jullie een idee te geven hoe je de code structureert.
+
+// API
+// fetchData (async)
+// Parameters: URL
+ 
+
+// APP
+// getUsers
+// Parameters: Zoekopdracht (Standaard leeg)
+// getUserDetail
+// getUserRepos
+// getUserFollowers
+// getCurrentWeather
+
+async function getCurrentWeather(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const url = "http://api.weatherapi.com/v1/current.json?key=b67bfa3fed5c439aa1883212230201&q=$ghent";
+getCurrentWeather(url).then(data => {
+  return `
+    <h2>Current weather in ${data.location.name}</h2>`
+});
+
+// getDogToilets
+ 
+
+// renderHTMLForUsers
+// renderHTMLForDogToilets
+...

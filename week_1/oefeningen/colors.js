7b1c8cd731eebe5a3017595c856f03a6e0232b17
pgm-jefdenie <jefdeniel@icloud.com>
2023-01-08 14:42:08 +0100

    change file structure

---
+++ week_1/oefeningen/colors.js
@@ -0,0 +1,24 @@
+// Maak een bestand colors.js aan.
+
+// Maak een array met verschillende kleuren aan, elke kleur heeft de volgende eigenschappen:
+
+// naam (string)
+// code (object)
+// rgba (string)
+// hsla (string)
+// hex (string)
+// Maak een object literal Color die deze eigenschappen ontvangt. Dit Color object heeft een functie genaamd getInfo(), deze functie zal een overzicht van de kleurcodes tonen.
+
+// output:
+
+// Name: Donkerblauw
+// ------------------------------
+// RGBA: rgba(0,0,0)
+// HSLA: hsla(100, 100%, 50%, 1)
+// HEX: #152648
+
+// Een color object bevat daarnaast ook een toString() functie die de naam en enkel de hex-value van het kleur teruggeeft.
+
+// Maak enkele instanties van Color met de colors array. Log de output van dit object naar de console.
+
+const colors = [];

5c04a0c083bd1121493554b47d3d1c81d324faab
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2023-01-09 14:26:44 +0100

    add files

---
+++ node_js/npm/2/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs"); // file system module
+// import fs from "fs" // Dit doet exact hetzelfde als hierboven,
+
+// The Node.js file system module allows you to work with the file system on your computer.
+// To include the File System module, use the require() method:
+
+console.log(fs);
+
+// The fs module has methods for creating new files:
+// fs.appendFile()
+// fs.open()
+// fs.writeFile()
